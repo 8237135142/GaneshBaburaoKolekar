4bf7c403c6b54b5d0d77a397db17df0b4eacdab1
Ganesh-IT <Ganesh-IT@DESKTOP-PTNUAPL>
2021-06-02 15:39:19 +0530

    commit

---
+++ BasicConcepts/src/com/hashmap/ClientSample.java
@@ -0,0 +1,32 @@
+package com.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class ClientSample {
+
+	public static void main(String[] args) {
+
+		Map<String, Employee> map = new HashMap<String, Employee>();
+		map.put("A", new Employee(101, "john", "consultent"));
+		map.put("b", new Employee(103, "asif", "Software Engineer"));
+		map.put("c", new Employee(102, "amir", "Software Developer"));
+
+		// getting entrySet() into Set
+		Set<Entry<String, Employee>> entrymap = map.entrySet();
+
+		for (Entry<String, Employee> entry : entrymap) {
+
+			String key = entry.getKey();
+
+			if (key.equalsIgnoreCase("b")) {
+				map.put("d", new Employee(105, "saif", "manager"));
+			} else {
+				System.out.println(entry.getKey() + "\t" + entry.getValue());
+			}
+		} 
+	}
+}
+
